83b4e1c3341d629b60bb3103426cc2d32f16877a
Ferhat Taş <ferhat.tas@arcelik.com>
2023-08-23 11:20:30 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ferhat Taş on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
